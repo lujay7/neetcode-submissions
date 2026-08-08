d91c0dd25db335609207f5ab2e00149a86c78934
David Lu <93889784+lujay7@users.noreply.github.com>
2026-08-08 15:15:04 -0400

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string>& strs)
+    {
+        if(strs.size() == 0)
+        {
+            return "";
+        }
+
+        string prefix = "";
+        for(int i = 0; i < strs[0].length(); i++)
+        {
+            for(int j = 0; j < strs.size()-1; j++)
+            {
+                if(strs[j][i] != strs[j+1][i])
+                {
+                    return prefix;
+                }
+            }
+            prefix = prefix + strs[0].substr(i,1);
+        }
+        return prefix;
+    }
+};
